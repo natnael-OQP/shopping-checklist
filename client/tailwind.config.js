39f77a853542749ddee71b781d515584e50e18ac
natnael-OQP <makiya112545@gmail.com>
2022-06-28 22:45:47 +0300

    config tailwind css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        popi: ["Poppins"],
-      },
-      colors: {
-        color1: "#64ffda",
-        color2: "#8892b0",
-        color3: "#ccd6f6",
-      },
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                popi: ['Poppins'],
+            },
+            colors: {
+                color1: '#F7F6FC',
+                color2: '#8892b0',
+                color3: '#656DF0',
+            },
+        },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
