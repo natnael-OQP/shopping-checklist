db33c77ead833a335ccbbc3c5a5846193a73641c
natnael-OQP <makiya112545@gmail.com>
2022-06-28 22:46:10 +0300

    chanege imageurl to image

--- client/src/Data.js
+++ client/src/Data.js
@@ -2,21 +2,28 @@ export const Items = [
     {
         id: 1,
         name: 'sony A7s ',
-        imageUrl:
-            'https://static.bhphoto.com/images/images500x500/sony_ilce7s_b_alpha_a7s_mirrorless_digital_1505306319_1044728.jpg',
+        image: 'https://static.bhphoto.com/images/images500x500/sony_ilce7s_b_alpha_a7s_mirrorless_digital_1505306319_1044728.jpg',
         price: 290,
         createdAt: 1,
         description:
-            'lorem jknasn jdsfh sd iueiqe :keoiqw eqpweoi acal; ;aksiwe .nnb uewyr  sd fjdsfh uwery aloie   sndbksdfhi weihsdfia weiuewury  kksfsdjfh weuywe  ',
+            "Free delivery on millions of items with Prime. Low prices across earth's biggest selection of books, music",
     },
     {
         id: 2,
         name: 'apple iphone 12 (white)',
-        imageUrl:
-            'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-white-select-2020?wid=470&hei=556&fmt=png-alpha&.v=1604343705000',
+        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-white-select-2020?wid=470&hei=556&fmt=png-alpha&.v=1604343705000',
         price: 999,
         createdAt: 2,
         description:
-            ' uewyr  sd fjdsfh uwery aloie   sndbksdfhi weihsdfia weiuewury  kksfsdjfh weuywe  ',
+            '  furniture, sporting goods, beauty & personal care, groceries & just about anything else.',
+    },
+    {
+        id: 3,
+        name: 'apple iphone 12 (white)',
+        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-white-select-2020?wid=470&hei=556&fmt=png-alpha&.v=1604343705000',
+        price: 999,
+        createdAt: 2,
+        description:
+            '  furniture, sporting goods, beauty & personal care, groceries & just about anything else.',
     },
 ]
