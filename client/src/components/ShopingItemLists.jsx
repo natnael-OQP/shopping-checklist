e386dbfef83eab71d396713c955168f8f1e1e913
natnael-OQP <makiya112545@gmail.com>
2022-06-28 22:47:54 +0300

    shopItem container

--- client/src/components/ShopingItemLists.jsx
+++ client/src/components/ShopingItemLists.jsx
@@ -1,15 +1,51 @@
-import { Items } from '../Data'
+// import { Items } from '../Data
 import ShopItem from './ShopItem'
+import AddItemModal from './AddItemModal'
+import { useEffect, useState } from 'react'
+import instance from '../config/axios'
 
 function ShoppingItemLists() {
+    const [showModal, setShowModal] = useState(false)
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        const fetch = async () => {
+            const { data } = await instance.get('/')
+            setItems(data)
+        }
+        fetch()
+    }, [])
+
+    console.log(items)
+
+    if (!items) {
+        return <h1>loading.....</h1>
+    }
     return (
-        <div className="max-w-3xl mx-auto   pt-20  ">
-            <h1 className="text-5xl font-popi font-semibold text-center mb-4">
+        <div className="max-w-3xl pt-20 mx-auto ">
+            <h1 className="text-5xl font-semibold text-center font-popi ">
                 Shopping item checklist
             </h1>
-            {Items.map((item) => (
-                <ShopItem key={item.id} item={item} />
-            ))}
+            <div className="my-4">
+                <button
+                    onClick={() => setShowModal(!showModal)}
+                    className="px-4 py-2 text-lg font-semibold rounded-md shadow-md bg-color3 text-color1 an font-popi "
+                >
+                    Add Items
+                </button>
+            </div>
+            <div className="flex flex-col flex-wrap w-full ">
+                {items.map((item) => (
+                    <ShopItem key={item.id} item={item} setItems={setItems} />
+                ))}
+            </div>
+            {showModal && (
+                <AddItemModal
+                    showModal={showModal}
+                    setShowModal={setShowModal}
+                    setItems={setItems}
+                />
+            )}
         </div>
     )
 }
