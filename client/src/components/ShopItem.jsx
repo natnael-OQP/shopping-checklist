7f1e41de5a7d6d37da07c2d8872f031638e6db94
natnael-OQP <makiya112545@gmail.com>
2022-06-28 22:41:34 +0300

    shopItem created

--- client/src/components/ShopItem.jsx
+++ client/src/components/ShopItem.jsx
@@ -1,28 +1,97 @@
 import { useState } from 'react'
-import { RiCloseCircleLine } from 'react-icons/ri'
-import { TiEdit } from 'react-icons/ti'
+import { MdDelete, MdEdit } from 'react-icons/md'
+import instance from '../config/axios'
 
-function Item({ item: { image, name, price, description, createdAt } }) {
+function Item({
+    item: { _id, image, name, price, desc, createdAt },
+    setItems,
+}) {
+    const [isChecked, setIsChecked] = useState(false)
+
+    const truncate = (str) => {
+        if (Number(str.length) >= 60) {
+            return str.substring(0, 70) + '...'
+        } else {
+            return str
+        }
+    }
+
+    const handelDelete = async () => {
+        // create optimistic ui for
+        setItems((prev) => prev.filter((item) => item?._id !== _id))
+        try {
+            await instance.delete(`/${_id}`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <div className=" p-4    bg-gray-200">
-            <div className="bg-white w-full flex flex-grow items-center">
+        <div className=" px-6 py-5 bg-[#EBECF4] rounded-md cursor-pointer">
+            <div
+                className={`${
+                    isChecked ? 'bg-violet-300' : 'bg-white'
+                }  w-full flex flex-grow items-center p-3 rounded-md shadow-md`}
+            >
                 <input
-                    className="   h-8 w-8 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                    className="float-left w-8 h-8 mt-1 mr-2 align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border border-gray-300 rounded-sm cursor-pointer checked:bg-blue-600 checked:border-blue-600 focus:outline-none"
                     type="checkbox"
+                    values={isChecked}
+                    onClick={() => setIsChecked(!isChecked)}
                 />
                 {image && (
-                    <div className="w-30 h-10">
-                        <img className="w-30 h-10" src={image} alt={name} />
-                    </div>
+                    <>
+                        <img
+                            className="h-20 bg-transparent w-30 "
+                            src={image}
+                            alt={name}
+                        />
+                    </>
                 )}
-                <div>
-                    <h3>{name}</h3>
-                    <h3>{price}</h3>
-                    <h3>{description}</h3>
+                <div className="w-full ml-3 ">
+                    {isChecked ? (
+                        <h3 className="text-base font-semibold font-popi ">
+                            <s>{name}</s>
+                        </h3>
+                    ) : (
+                        <h3 className="text-base font-semibold font-popi ">
+                            {name}
+                        </h3>
+                    )}
+
+                    {isChecked ? (
+                        <h3 className="py-1 text-sm font-medium text-gray-500 font-popi">
+                            <s>{truncate(desc)}</s>
+                        </h3>
+                    ) : (
+                        <h3 className="py-1 text-sm font-medium text-gray-500 font-popi">
+                            {truncate(desc)}
+                        </h3>
+                    )}
+
+                    {isChecked ? (
+                        <h3 className="text-xs font-medium text-gray-500 font-popi">
+                            <s>
+                                {price}{' '}
+                                <span className="text-[10px] ">birr</span>
+                            </s>
+                        </h3>
+                    ) : (
+                        <h3 className="text-xs font-medium text-gray-500 font-popi">
+                            {price} <span className="text-[10px] ">birr</span>
+                        </h3>
+                    )}
                 </div>
-                <div>
-                    <RiCloseCircleLine />
-                    <TiEdit />
+                <div className="flex flex-col ">
+                    <button
+                        onClick={handelDelete}
+                        className="bg-[#EBECF4] m-1 p-[5px] hover:scale-105 rounded-sm active:scale-100 hover:sm"
+                    >
+                        <MdDelete size={24} color="#333" />
+                    </button>
+                    <button className="bg-[#EBECF4] m-1 p-[5px] hover:scale-105 rounded-sm active:scale-100 hover:sm">
+                        <MdEdit size={24} color="#333" />
+                    </button>
                 </div>
             </div>
         </div>
