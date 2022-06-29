4fd5d273398065165da19df07424a1f16731a722
natnael-OQP <makiya112545@gmail.com>
2022-06-29 15:38:50 +0300

    task 3 completed

--- client/src/components/ShopItem.jsx
+++ client/src/components/ShopItem.jsx
@@ -2,18 +2,32 @@ import { useState } from 'react'
 import { MdDelete, MdEdit } from 'react-icons/md'
 import instance from '../config/axios'
 
+import moment from 'moment'
+
 function Item({
-    item: { _id, image, name, price, desc, createdAt },
+    item: { _id, image, name, price, desc, createdAt, isChecked, updatedAt },
     setItems,
     handelUpdate,
 }) {
-    const [isChecked, setIsChecked] = useState(false)
+    const [check, setIsChecked] = useState(isChecked)
 
     const truncate = (str) => {
         if (Number(str.length) >= 60) {
             return str.substring(0, 70) + '...'
         } else {
             return str
+        }
+    }
+
+    const handelChecked = async () => {
+        const ischecked = !check
+        setIsChecked(!check)
+        try {
+            await instance.put(`/${_id}`, {
+                isChecked: ischecked,
+            })
+        } catch (error) {
+            console.log(error)
         }
     }
 
@@ -31,14 +45,15 @@ function Item({
         <div className=" px-6 py-5 bg-[#EBECF4] rounded-md cursor-pointer">
             <div
                 className={`${
-                    isChecked ? 'bg-violet-300' : 'bg-white'
-                }  w-full flex flex-grow items-center p-3 rounded-md shadow-md`}
+                    check ? 'bg-violet-300' : 'bg-white'
+                }  w-full flex flex-grow items-center p-3 rounded-md shadow-md relative`}
             >
                 <input
                     className="float-left w-8 h-8 mt-1 mr-2 align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border border-gray-300 rounded-sm cursor-pointer checked:bg-blue-600 checked:border-blue-600 focus:outline-none"
                     type="checkbox"
-                    values={isChecked}
-                    onClick={() => setIsChecked(!isChecked)}
+                    checked={check}
+                    values={check}
+                    onChange={handelChecked}
                 />
                 {image && (
                     <>
@@ -50,7 +65,7 @@ function Item({
                     </>
                 )}
                 <div className="w-full ml-3 ">
-                    {isChecked ? (
+                    {check ? (
                         <h3 className="text-base font-semibold font-popi ">
                             <s>{name}</s>
                         </h3>
@@ -60,7 +75,7 @@ function Item({
                         </h3>
                     )}
 
-                    {isChecked ? (
+                    {check ? (
                         <h3 className="py-1 text-sm font-medium text-gray-500 font-popi">
                             <s>{truncate(desc)}</s>
                         </h3>
@@ -70,7 +85,7 @@ function Item({
                         </h3>
                     )}
 
-                    {isChecked ? (
+                    {check ? (
                         <h3 className="text-xs font-medium text-gray-500 font-popi">
                             <s>
                                 {price}{' '}
@@ -91,12 +106,18 @@ function Item({
                         <MdDelete size={24} color="#333" />
                     </button>
                     <button
-                        // onClick={() => handelUpdate(_id)}
+                        onClick={() => handelUpdate(_id)}
                         className="bg-[#EBECF4] m-1 p-[5px] hover:scale-105 rounded-sm active:scale-100 hover:sm"
                     >
                         <MdEdit size={24} color="#333" />
                     </button>
                 </div>
+                <span className="text-xs font-medium text-slate-400 font-popi text-[10px] absolute bottom-0 left-5 ">
+                    created at : {moment(createdAt).fromNow()}
+                </span>
+                <span className="text-xs font-medium text-slate-400 font-popi text-[10px] absolute bottom-0 right-5 ">
+                    updated at : {moment(updatedAt).fromNow()}
+                </span>
             </div>
         </div>
     )
